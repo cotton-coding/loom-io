8fb76ca054bdc716e5738a54ab57892cfce32664
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-03-12 20:41:23 +0100

    fix type is prefixed by $

--- plugins/jsonConverter/src/jsonConverter.ts
+++ plugins/jsonConverter/src/jsonConverter.ts
@@ -1,7 +1,7 @@
 import { PLUGIN_TYPE, type LoomFileConverter } from '@loom-io/core';
 
 export default {
-	type: PLUGIN_TYPE.FILE_CONVERTER,
+	$type: PLUGIN_TYPE.FILE_CONVERTER,
 	extensions: ['json'],
 	parse: JSON.parse,
 	stringify: JSON.stringify
