3bc6a4fae6b222df33311ecd3028ca7748fe753e
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-04-01 15:34:05 +0200

    add tests for isFuntions

--- packages/core/src/exports/lib.spec.ts
+++ packages/core/src/exports/lib.spec.ts
@@ -1,7 +1,10 @@
 import { expect, test, describe, afterEach, vi } from 'vitest';
-import { LoomIO, LoomSourceAdapter, NoSourceAdapterException, PLUGIN_TYPE, SourceAdapter } from './lib.js';
+import { LoomIO, LoomSourceAdapter, NoSourceAdapterException, PLUGIN_TYPE, SourceAdapter, isFile, isList, isDirectory, isEditor, isReader, ObjectDirentInterface, FileHandler } from './lib.js';
+import { List } from '../core/list.js';
+import { Editor } from '../core/editor.js';
 import { Directory } from '../core/dir.js';
 import { LoomFile } from '../core/file.js';
+import { FileHandle } from 'fs/promises';
 
 
 class LoomFsTest extends LoomIO {
@@ -86,5 +89,91 @@ describe('Test Entry', () => {
 		await expect(LoomIO.file('test://data/test.json')).rejects.toThrowError(NoSourceAdapterException);
 		await expect(LoomIO.source('test://data')).rejects.toThrowError(NoSourceAdapterException);
 	});
+
+	describe.concurrent('Type Check', () => {
+		test('isFile definition', () => {
+			expect(isFile).toBeDefined();
+			expect(isFile({})).toBeFalsy();
+		});
+
+		test('isFile and no other', async () => {
+			LoomIO.register(sourceAdapter);
+			const file = await LoomIO.file('test://data/test.json');
+			expect(isFile(file)).toBeTruthy();
+			expect(isList(file)).toBeFalsy();
+			expect(isDirectory(file)).toBeFalsy();
+			expect(isEditor(file)).toBeFalsy();
+			expect(isReader(file)).toBeFalsy();
+		});
+
+		test('directory is no file', async () => {
+			LoomIO.register(sourceAdapter);
+			const dir = await LoomIO.dir('test://data');
+			expect(isFile(dir)).toBeFalsy();
+		});
+
+		test('isList definition', () => {
+			expect(isList).toBeDefined();
+			expect(isList({})).toBeFalsy();
+		});
+
+		test('isList and no other', async () => {
+			LoomIO.register(sourceAdapter);
+			const dir = await LoomIO.dir('test://');
+			const list = new List(dir, []);
+			expect(isList(list)).toBeTruthy();
+			expect(isFile(list)).toBeFalsy();
+			expect(isDirectory(list)).toBeFalsy();
+			expect(isEditor(list)).toBeFalsy();
+			expect(isReader(list)).toBeFalsy();
+		});
+
+
+		test('isDirectory definition', () => {
+			expect(isDirectory).toBeDefined();
+			expect(isDirectory({})).toBeFalsy();
+		});
+
+		test('isDirectory no other', async () => {
+			LoomIO.register(sourceAdapter);
+			const dir = await LoomIO.dir('test://data');
+			expect(isDirectory(dir)).toBeTruthy();
+			expect(isFile(dir)).toBeFalsy();
+			expect(isList(dir)).toBeFalsy();
+			expect(isEditor(dir)).toBeFalsy();
+			expect(isReader(dir)).toBeFalsy();
+		});
+
+		test('isEditor definition', () => {
+			expect(isEditor).toBeDefined();
+			expect(isEditor({})).toBeFalsy();
+		});
+
+		test('isEditor no other', async () => {
+			LoomIO.register(sourceAdapter);
+			const file = await LoomIO.file('test://data/test.json');
+			const editor = new Editor(file, {} as FileHandler);
+			expect(isEditor(editor)).toBeTruthy();
+			expect(isFile(editor)).toBeFalsy();
+			expect(isList(editor)).toBeFalsy();
+			expect(isDirectory(editor)).toBeFalsy();
+			expect(isReader(editor)).toBeTruthy();
+		});
+
+		test('isReader definition', () => {
+			expect(isReader).toBeDefined();
+			expect(isReader({})).toBeFalsy();
+		});
+
+		test('editor is reader', async () => {
+			LoomIO.register(sourceAdapter);
+			const file = await LoomIO.file('test://data/test.json');
+			const editor = new Editor(file, {} as FileHandler);
+			expect(isReader(editor)).toBeTruthy();
+		});
+	});
+
+
+
 
 });
