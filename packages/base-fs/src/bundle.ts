fd939425f125dcee74e7dd00b660e67f28bdde65
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-05-12 19:41:48 +0200

    fix base-fs

--- packages/base-fs/src/bundle.ts
+++ packages/base-fs/src/bundle.ts
@@ -1,12 +1,11 @@
 import LoomIO from '@loom-io/core';
 import jsonConverter from '@loom-io/json-converter';
 import yamlConverter from '@loom-io/yaml-converter';
-import fileSystemAdapter from '@loom-io/node-filesystem-adapter';
+import { FilesystemAdapter } from '@loom-io/node-filesystem-adapter';
 
-LoomIO.register(fileSystemAdapter('file://'));
 LoomIO.register(yamlConverter());
 LoomIO.register(jsonConverter());
 
 export * from '@loom-io/core';
 
-export default LoomIO;
+export default FilesystemAdapter;
