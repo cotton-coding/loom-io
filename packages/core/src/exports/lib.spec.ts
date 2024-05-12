91bf42270c90f51d4b71d947f32e218e7df9b5ba
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-05-12 19:24:59 +0200

    Cleanup loom

--- packages/core/src/exports/lib.spec.ts
+++ packages/core/src/exports/lib.spec.ts
@@ -1,5 +1,5 @@
-import { expect, test, describe, afterEach, vi } from 'vitest';
-import { LoomIO, LoomSourceAdapter, NoSourceAdapterException, PLUGIN_TYPE, SourceAdapter, isFile, isList, isDirectory, isEditor, isReader, FileHandler } from './lib.js';
+import { expect, test, describe, afterEach } from 'vitest';
+import { LoomIO, SourceAdapter, isFile, isList, isDirectory, isEditor, isReader, FileHandler } from './lib.js';
 import { List } from '../core/list.js';
 import { Editor } from '../core/editor.js';
 import { Directory } from '../core/dir.js';
@@ -10,7 +10,6 @@ class LoomFsTest extends LoomIO {
 
 	static clean() {
 		LoomIO.pluginNonces = [];
-		LoomIO.sourceAdapters = [];
 	}
 
 	static getPluginHashes() {
@@ -18,30 +17,6 @@ class LoomFsTest extends LoomIO {
 	}
 }
 
-const mockSourceAdapter =
-	vi.fn((link: string, Type: typeof Directory | typeof LoomFile) => {
-		if(link.startsWith('test://')) {
-			const adapter = {} as SourceAdapter;
-			if(Type === Directory) {
-				return new Directory(adapter, link);
-			} else if(Type === LoomFile) {
-				const dir = new Directory(adapter, link);
-				return new LoomFile(adapter, dir, link);
-			}
-		}
-	}) as LoomSourceAdapter['source'];
-
-const sourceAdapter: LoomSourceAdapter = {
-	$type: PLUGIN_TYPE.SOURCE_ADAPTER,
-	source: mockSourceAdapter,
-	nonce: Symbol()
-};
-
-const sourceAdapter2: LoomSourceAdapter = {
-	$type: PLUGIN_TYPE.SOURCE_ADAPTER,
-	source: () => mockSourceAdapter('fixture://test', Directory),
-	nonce: Symbol()
-};
 
 describe('Test Entry', () => {
 
@@ -49,56 +24,6 @@ describe('Test Entry', () => {
 		LoomFsTest.clean();
 	});
 
-	test('Has Statics to create Dir and Files', () => {
-		expect(LoomIO).toBeDefined();
-		expect(LoomIO.dir).toBeDefined();
-		expect(LoomIO.file).toBeDefined();
-	});
-
-	test('Register Plugins', () => {
-		const pluginHashes = LoomFsTest.getPluginHashes();
-		expect(pluginHashes).toHaveLength(0);
-	});
-
-	test('Register Plugins only once', () => {
-		const pluginHashes = LoomFsTest.getPluginHashes();
-		expect(pluginHashes).toHaveLength(0);
-		LoomIO.register(sourceAdapter);
-		expect(pluginHashes).toHaveLength(1);
-		LoomIO.register(sourceAdapter);
-		expect(pluginHashes).toHaveLength(1);
-		LoomIO.register(sourceAdapter2);
-		expect(pluginHashes).toHaveLength(2);
-		LoomIO.register(sourceAdapter);
-		expect(pluginHashes).toHaveLength(2);
-	});
-
-	test('Get Dir Instance', async () => {
-		LoomIO.register(sourceAdapter);
-		const dir = await LoomIO.dir('test://test');
-		expect(dir).toBeDefined();
-		expect(dir).toBeInstanceOf(Directory);
-	});
-
-	test('Get File Instance', async () => {
-		LoomIO.register(sourceAdapter);
-		const file = await LoomIO.file('test://data/test.json');
-		expect(file).toBeDefined();
-		expect(file).toBeInstanceOf(LoomFile);
-	});
-
-	test('No Source Adapter found', async () => {
-		LoomIO.register(sourceAdapter);
-		await expect(LoomIO.dir('http://test.com/dir')).rejects.toThrowError(NoSourceAdapterException);
-		await expect(LoomIO.file('http://test.com/file.txt')).rejects.toThrowError(NoSourceAdapterException);
-		await expect(LoomIO.source('http://test.com')).rejects.toThrowError(NoSourceAdapterException);
-	});
-
-	test('No Source Adapter registered', async () => {
-		await expect(LoomIO.dir('test://data')).rejects.toThrowError(NoSourceAdapterException);
-		await expect(LoomIO.file('test://data/test.json')).rejects.toThrowError(NoSourceAdapterException);
-		await expect(LoomIO.source('test://data')).rejects.toThrowError(NoSourceAdapterException);
-	});
 
 	describe.concurrent('Type Check', () => {
 		test('isFile definition', () => {
@@ -107,8 +32,7 @@ describe('Test Entry', () => {
 		});
 
 		test('isFile and no other', async () => {
-			LoomIO.register(sourceAdapter);
-			const file = await LoomIO.file('test://data/test.json');
+			const file = new LoomFile({} as SourceAdapter, {} as Directory, 'test.json');
 			expect(isFile(file)).toBeTruthy();
 			expect(isList(file)).toBeFalsy();
 			expect(isDirectory(file)).toBeFalsy();
@@ -117,8 +41,7 @@ describe('Test Entry', () => {
 		});
 
 		test('directory is no file', async () => {
-			LoomIO.register(sourceAdapter);
-			const dir = await LoomIO.dir('test://data');
+			const dir = new Directory({} as SourceAdapter, 'test://data');
 			expect(isFile(dir)).toBeFalsy();
 		});
 
@@ -128,8 +51,7 @@ describe('Test Entry', () => {
 		});
 
 		test('isList and no other', async () => {
-			LoomIO.register(sourceAdapter);
-			const dir = await LoomIO.dir('test://');
+			const dir = new Directory({} as SourceAdapter, 'test://data');
 			const list = new List(dir, []);
 			expect(isList(list)).toBeTruthy();
 			expect(isFile(list)).toBeFalsy();
@@ -145,8 +67,7 @@ describe('Test Entry', () => {
 		});
 
 		test('isDirectory no other', async () => {
-			LoomIO.register(sourceAdapter);
-			const dir = await LoomIO.dir('test://data');
+			const dir = new Directory({} as SourceAdapter, 'test://data');
 			expect(isDirectory(dir)).toBeTruthy();
 			expect(isFile(dir)).toBeFalsy();
 			expect(isList(dir)).toBeFalsy();
@@ -160,8 +81,7 @@ describe('Test Entry', () => {
 		});
 
 		test('isEditor no other', async () => {
-			LoomIO.register(sourceAdapter);
-			const file = await LoomIO.file('test://data/test.json');
+			const file = new LoomFile({} as SourceAdapter, {} as Directory, 'test.json');
 			const editor = new Editor(file, {} as FileHandler);
 			expect(isEditor(editor)).toBeTruthy();
 			expect(isFile(editor)).toBeFalsy();
@@ -176,8 +96,7 @@ describe('Test Entry', () => {
 		});
 
 		test('editor is reader', async () => {
-			LoomIO.register(sourceAdapter);
-			const file = await LoomIO.file('test://data/test.json');
+			const file = new LoomFile({} as SourceAdapter, {} as Directory, 'test.json');
 			const editor = new Editor(file, {} as FileHandler);
 			expect(isReader(editor)).toBeTruthy();
 		});
