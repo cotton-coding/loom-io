d478ea360abbdbc36e6dba9835f9843ab115e3fa
Wolfgang Rathgeb <W.Rathgeb@dropanote.de>
2024-07-01 23:28:47 +0200

    Add function to get full path from filesystem adapter (#35)
    * add function to get the full path
    
    * check for same adapter by default
    
    * add documentaton
    
    * update version

---
+++ adapters/node-fs/src/exports/lib.spec.ts
@@ -0,0 +1,59 @@
+import { describe, test, expect } from "vitest";
+import { FilesystemAdapter } from "./lib";
+
+describe("FilesystemAdapter", () => {
+	test("should be able to create a new instance", () => {
+		const adapter = new FilesystemAdapter();
+		expect(adapter).toBeDefined();
+	});
+
+	test("fullPath should return the full path of a file", () => {
+		const adapter = new FilesystemAdapter("/root/dir");
+		const file = adapter.file("file.txt");
+		expect(adapter.getFullPath(file)).toBe("/root/dir/file.txt");
+	});
+
+	test("fullPath should return the full path of a directory", () => {
+		const adapter = new FilesystemAdapter("/etc/loom");
+		const dir = adapter.dir("dir");
+		expect(adapter.getFullPath(dir)).toBe("/etc/loom/dir");
+	});
+
+	test("fullPath should fail to other adapters", () => {
+		const adapter = new FilesystemAdapter("/etc/loom");
+		const adapter2 = new FilesystemAdapter("/etc/loom");
+		const dir2 = adapter2.dir("dir");
+		expect(() => adapter.getFullPath(dir2)).toThrowError();
+	});
+
+	test("fullPath should return the full path also from other adapter if flag is set", () => {
+		const adapter = new FilesystemAdapter("/etc/loom");
+		const adapter2 = new FilesystemAdapter("/etc/loom");
+		const dir2 = adapter2.dir("dir");
+		expect(adapter.getFullPath(dir2, true)).toBe("/etc/loom/dir");
+	});
+
+	test("fullPath should return the full path of a file (cwd)", () => {
+		const adapter = new FilesystemAdapter();
+		const file = adapter.file("file.txt");
+		expect(adapter.getFullPath(file)).toBe(process.cwd() + "/file.txt");
+	});
+
+	test("fullPath should return the full path of a directory (cwd)", () => {
+		const adapter = new FilesystemAdapter();
+		const dir = adapter.dir("dir");
+		expect(adapter.getFullPath(dir)).toBe(process.cwd() + "/dir");
+	});
+
+	test("file should return a new LoomFile instance", () => {
+		const adapter = new FilesystemAdapter();
+		const file = adapter.file("file.txt");
+		expect(file).toBeDefined();
+	});
+
+	test("dir should return a new Directory instance", () => {
+		const adapter = new FilesystemAdapter();
+		const dir = adapter.dir("dir");
+		expect(dir).toBeDefined();
+	});
+});
